2cab32c9396ddc3e3d128ec6ae0c16eb76a68256
Liza <e.a.tolstonogova@mail.ru>
2023-08-10 18:46:10 +0700

    Занятие 7 дз

--- mysite/advertisements/models.py
+++ mysite/advertisements/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=80, verbose_name="заголовок")
+    description = models.TextField("описание")
+    price = models.DecimalField("цена", max_digits=12, decimal_places=2)
+    auction = models.BooleanField("возможность торга", help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField("дата создания", auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "advertisements"
+
+    def __str__(self):
+        return f"<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})"
