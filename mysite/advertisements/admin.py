ebcfeaad649a8a77c97c4f47a1a9444a33c7e801
Liza <e.a.tolstonogova@mail.ru>
2023-08-28 16:14:00 +0700

    Занятие 10 дз

--- mysite/advertisements/admin.py
+++ mysite/advertisements/admin.py
@@ -1,37 +1,23 @@
 from django.contrib import admin
 from .models import Advertisement
-from django.utils.html import format_html
 
+
+# Register your models here.
 
 @admin.register(Advertisement)
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "auction", "price", "created_date","created_update", "created_image"]
-    list_filter = ["auction", "created_at"]
-    search_fields = ["title"]
+    list_display = ['id', 'title', 'price', 'create_date', 'update_date', 'auction', 'view_image']
+    list_filter = ['auction',"image"]
     actions = ["mark_auction_as_true", "mark_auction_as_false"]
-
-    fieldsets=(
-        ('Общая информация',{
-           "fields": ("title","description","user", "image"),
-           "classes":["collapse"]
-        }),
-        ('Финансы',{
-           "fields": ("price","auction"),
-           "classes":["collapse"]           
-        }),        
+    fieldsets = (
+        ("Общее", {'fields': ('title', 'description', 'user', 'image'), 'classes': ['collapse']}),
+        ("Финансы", {'fields': ('price', 'auction'), 'classes': ['collapse']})
     )
 
-
-    @admin.action(description="Добавить возможность торга")
+    @admin.action(description='Добавить возможность торга')
     def mark_auction_as_true(self, request, queryset):
         queryset.update(auction=True)
 
-    @admin.action(description="Убрать возможность торга")
+    @admin.action(description='Убрать возможность торга')
     def mark_auction_as_false(self, request, queryset):
         queryset.update(auction=False)
-    
-
-
-
-
-# admin.site.register(Advertisement, AdvertisementAdmin)
